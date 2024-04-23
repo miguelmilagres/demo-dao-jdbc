1eda8460b2e30b5d7ab19c703fbfebaba9864788
miguelmilagres <miguelmdemacedo@gmail.com>
2024-04-23 11:11:27 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TESTE 1: seller findById ====");
-		Seller seller = sellerDao.findById(3);
+		Seller seller = sellerDao.findById(1);
 				
 		System.out.println(seller);
 		
@@ -36,6 +36,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TESTE 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println(seller);
 	}
 
 }
